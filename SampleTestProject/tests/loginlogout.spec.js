c0140f6f4db0a9918f2769b1bae48d19dd78ff06
pgudi <prabhakargudi@gmail.com>
2026-02-14 15:56:22 +0530

    Login Actions has done

--- SampleTestProject/tests/loginlogout.spec.js
+++ SampleTestProject/tests/loginlogout.spec.js
@@ -2,11 +2,14 @@ const {test, expect} = require('@playwright/test');
 
 test("Login into The Application", async({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    await page.waitForTimeout(3000)
     const title=await page.title()
     await expect(title==="OrangeHRM").toBeTruthy()
     await expect(title.includes("Orange")).toBeTruthy()
     await expect(title).toContain("Orange")
     await expect(page).toHaveTitle("OrangeHRM")
+    await page.locator("//input[@name='username']").fill("Admin")
+    await page.locator("//input[@name='password']").fill("admin123")
+    await page.locator("//button[@type='submit']").click()
+    await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
 
 })
